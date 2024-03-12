46865bb53b62170aa621819a6cf993482b2b273c
Ryan Denninger <ryandenninger7@gmail.com>
2024-03-11 21:09:50 -0500

    appcommit1

---
+++ Starter_Code/StarterCode/static/js/app.js
@@ -0,0 +1,88 @@
+let init = () => {
+  let selector = d3.select("#selDataset");
+
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    let sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+};
+
+let buildMetadata = (sample) => {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    let metadata = data.metadata;
+    let result = metadata.filter(sampleObj => sampleObj.id == sample)[0];
+    let PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+};
+
+let buildCharts = (sample) => {
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    let samples = data.samples;
+    let result = samples.filter(sampleObj => sampleObj.id == sample)[0];
+
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
+
+    // Bar Chart
+    let barData = [{
+      x: sample_values.slice(0, 10).reverse(),
+      y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
+    }];
+
+    let barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot("bar", barData, barLayout);
+
+    // Bubble Chart
+    let bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
+    let bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }];
+
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+  });
+};
+
+let optionChanged = (newSample) => {
+  buildCharts(newSample);
+  buildMetadata(newSample);
+};
+
+init();
+
+
